a6653ab76f19ef0f86686ac562ecefc2fd04bb5d
Mamadou mbaye wade <mamadou.mb.wade@gmail.com>
2021-12-02 00:32:14 +0000

    second commit

---
+++ client/src/components/message/Message.jsx
@@ -0,0 +1,14 @@
+import "./message.css";
+import { format } from "timeago.js";
+
+export default function Message({ message, own }) {
+  return (
+    <div className={own ? "message own" : "message"}>
+      <div className="messageTop">
+        <img src="/assets/person/2.jpeg" className="messageImg" alt="" />
+        <p className="messageText">{message.text}</p>
+      </div>
+      <div className="messageBottom">{format(message.createdAt)}</div>
+    </div>
+  );
+}
